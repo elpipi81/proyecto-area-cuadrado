d12b2e920a2297a448000740f9d7c3499d7c70b2
Hernan <161507374+elpipi81@users.noreply.github.com>
2024-09-19 13:40:22 +0200

    Update area_cuadrado.py

--- area_cuadrado.py
+++ area_cuadrado.py
@@ -1,1 +1,17 @@
+# Calcula el area de un cuadrado
 
+# Solicitamos el dato al usuario
+lado = float(input("Ingrese en centimetros la longitud de un lado del cuadrado: "))
+
+# Calculamos el area del cuadrado
+area = lado * lado
+
+# Mostramos el resultado
+print(f"El area del cuadrado con lado {lado} es: {area}.")
+
+# Tambien podemos calcular el perimetro
+print("Tambien podemos calcular el perimetro del cuadrado...")
+
+perimetro = lado * 4
+
+print(f"El perimetro del cuadrado con lado {lado} es: {perimetro}.")
